87d7d43b3dfdb13c148a8fe44dc4aaa0688d590b
Michel Oliveira <developer.micheloliveira@gmail.com>
2024-08-16 14:07:22 -0300

    Banco de dados
    Adicionada a configuração inicial do banco de dados do projeto.

---
+++ src/config/database.php
@@ -0,0 +1,17 @@
+<?php 
+
+class Database
+{
+    public static function getConnection()
+    {
+        $envPath = realpath(dirname(__FILE__).'/../../.env');
+        $env = parse_ini_file($envPath);
+        $conn = new mysqli($env['DB_HOST'], $env['DB_USER'], $env['DB_PASSWORD'], $env['DB_DATABASE']);
+
+        if ($conn->connect_error) {
+            die('Erro: '.$conn->connect_error);
+        }
+
+        return $conn;
+    }
+}
