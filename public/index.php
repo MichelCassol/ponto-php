21618257c2d04654c5bcabd0bd4914161ce4e0c1
Michel Oliveira <developer.micheloliveira@gmail.com>
2024-08-22 13:54:37 -0300

    Estrutura/models
    Inseridas funcionalidas para manipulação dos dados de usuário no model.

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php 
 
-require_once(dirname(__FILE__,2).'/src/config/config.php');
+require_once(dirname(__FILE__, 2).'/src/config/config.php');
+require_once(dirname(__FILE__, 2).'/src/models/User.php');
 
 Database::getConnection();
