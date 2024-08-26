8468e679dee8fad99a20e1ed1b4a8799cf928b52
Michel Oliveira <developer.micheloliveira@gmail.com>
2024-08-26 14:06:07 -0300

    Criada a página de login

--- public/index.php
+++ public/index.php
@@ -1,12 +1,4 @@
 <?php 
 
 require_once(dirname(__FILE__, 2).'/src/config/config.php');
-require_once(dirname(__FILE__, 2).'/src/models/User.php');
-
-$user = new User(['name' => 'Michel','email' => 'michel@gmail.com']);
-
-// echo $user->getSelect();
-
-print_r(User::get(['id' => '1'], "name, email"));
-print_r(User::get(['name' => 'Chaves'], "id, name, email"));
-print_r(User::get());
+require_once(dirname(__FILE__, 2).'/src/views/login.php');
