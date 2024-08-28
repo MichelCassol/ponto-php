d047d4421a5181b74fde33e72fc93ca29ef0d6e8
Michel Oliveira <michelcssoliveira@gmail.com>
2024-08-27 22:36:02 -0300

    Adicionado o controller do login e alterado o mapeamento do index.php para ele.

--- src/config/config.php
+++ src/config/config.php
@@ -5,8 +5,8 @@ setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'portuguese');
 // Pastas
 define('MODEL_PATH', realpath(dirname(__FILE__, 2) . '/models'));
 define('VIEW_PATH', realpath(dirname(__FILE__, 2) . '/views'));
-define('CONTROLLER_PATH', realpath(dirname(__FILE__, 2) . '/controller'));
+define('CONTROLLER_PATH', realpath(dirname(__FILE__, 2) . '/controllers'));
 
 // Arquivos
 require_once(realpath(dirname(__FILE__) . '/database.php'));
-require_once(realpath(dirname(__FILE__).'/autoload.php'));
+require_once(realpath(dirname(__FILE__) . '/autoload.php'));
