036294b68564551266423b27b46ecceeac5e2d81
Michel Oliveira <developer.micheloliveira@gmail.com>
2024-08-29 14:08:35 -0300

    Adicionada a classe de exception e configurado o envio de dados do formulário de login.

--- src/controllers/loginController.php
+++ src/controllers/loginController.php
@@ -1,2 +1,13 @@
 <?php
-echo 'Login controllers'; 
+
+if (count($_POST) > 0) {
+    $login = new Login($_POST);
+    try {
+        $user = $login->checkLogin();
+        echo $user->name;
+    } catch (Exception $th) {
+        echo 'Falha no login';
+    }
+}
+
+loadView('login', $_POST);
