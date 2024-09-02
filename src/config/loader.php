4d7d61e32267ddeb3267223bcb764f1512b53ba8
Michel Oliveira <developer.micheloliveira@gmail.com>
2024-09-02 14:03:19 -0300

    Adicionada a estrutura para a tela de registro de ponto.

--- src/config/loader.php
+++ src/config/loader.php
@@ -16,3 +16,15 @@ function loadView(string $viewName, array $params = [])
     }
     require_once(VIEW_PATH . "/$viewName.php");
 }
+
+function loadTemplateView(string $viewName, array $params = [])
+{
+    if (count($params) > 0) {
+        foreach ($params as $key => $value) {
+            if (strlen($key) > 0) {
+                $$key = $value;
+            }
+        }
+    }
+    require_once(VIEW_PATH . "/$viewName.php");
+}
