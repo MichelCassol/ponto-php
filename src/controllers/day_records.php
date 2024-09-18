ddecafb4d3f610cff1b67093633bdc1f70c35ab9
Michel Oliveira <developer.micheloliveira@gmail.com>
2024-09-18 14:10:09 -0300

    Adicionado template de dados e configurada a data da interface

--- src/controllers/day_records.php
+++ src/controllers/day_records.php
@@ -3,7 +3,8 @@ session_start();
 requireValidSession();
 
 $date = (new DateTime())->getTimestamp();
-$today = date("j \d\\e F \d\\e Y", $date);
+// $today = date("j \d\\e F \d\\e Y", $date);
+$today = strftime('%d de %B de %Y',$date);
 
 loadTemplateView('day_records', ['today' => $today]);
 
