9aa197667c536fc5abe4cc31932c54b6923beab8
Michel Oliveira <developer.micheloliveira@gmail.com>
2024-10-10 14:15:39 -0300

    Adicionada a função que atualiza os timers na interface automativamente

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -5,3 +5,31 @@
 		body.classList.toggle('hide-sidebar')
 	}
 })()
+
+function addOneSeconds(hours, minutes, seconds) 
+{
+	const d = new Date()
+	d.setHours(parseInt(hours))
+	d.setMinutes(parseInt(minutes))
+	d.setSeconds(parseInt(seconds) + 1)
+
+	const h = `${d.getHours()}`.padStart(2, 0)
+	const m = `${d.getMinutes()}`.padStart(2, 0)
+	const s = `${d.getSeconds()}`.padStart(2, 0)
+
+	return `${h}:${m}:${s}`
+}
+
+function activateClock() 
+{
+	const activateClock = document.querySelector('[active-clock]')
+	if (!activateClock) return
+	
+	setInterval(function () {
+		const parts = activateClock.innerHTML.split(':')
+		activateClock.innerHTML = addOneSeconds(...parts)
+	}, 1000)
+	
+}
+
+activateClock()
