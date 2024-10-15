f707a217c5991a3f301c37a3938ac609049d4dee
Michel Oliveira <developer.micheloliveira@gmail.com>
2024-10-15 14:07:37 -0300

    Criadas novas funções para manipulação de datas no relatório mensal.

--- src/controllers/monthly_report.php
+++ src/controllers/monthly_report.php
@@ -2,11 +2,38 @@
 session_start();
 requireValidSession();
 
+$currentDate = new DateTime();
+
 $user = $_SESSION['user'];
+$registries = WorkingHours::getMonthlyReport($user->id, $currentDate);
 
-$registries = WorkingHours::getMonthlyReport($user->id, new DateTime());
+$report = [];
+$workDay = 0;
+$sumOfWorkedTime = 0;
+$lastDay = getLastDayOfMonth($currentDate)->format('d');
 
+for ($day = 1; $day <= $lastDay; $day++) {
+    $date = $currentDate->format('Y-m') . '-' . sprintf('%02d', $day);
+    $registry = $registries[$date];
+
+    if (isPastWorkday($date)) $workDay++;
+
+    if ($registry) {
+        $sumOfWorkedTime += $registry->worked_time;
+        $report[] = $registry;
+    } else {
+        $report[] = new WorkingHours([
+            'work_date' => $date,
+            'worked_time' => 0
+        ]);
+    }
+}
+
+$expectedTime = $workDay * DAILY_TIME;
+$balance = getTimeStringFromSeconds(abs($sumOfWorkedTime - $expectedTime));
 
 loadTemplateView('monthly_report', [
-    'registries' => $registries
+    'report' => $report,
+    'sumOfWorkedTime' => $sumOfWorkedTime,
+    'balance' => $balance
 ]);
